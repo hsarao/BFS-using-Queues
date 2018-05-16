18ad7c39f0c754ee4d11a8cb9a44285e09b9314a
Jasleen Sarao <jslnsran@gmail.com>
2018-05-16 15:20:59 -0500

    Add project files.

---
+++ Assignment2/Point.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment2
+{
+    public class Point
+    {
+        public int Row { get; }
+        public int Column { get; }
+
+        public Point(int row, int column)
+        {
+            this.Row = row;
+            this.Column = column;
+        }
+
+        //public int getRow()
+        //{
+        //    return this.row;
+        //}
+
+        //public int getColumn()
+        //{
+        //    return this.column;
+        //}
+
+        override
+        public String ToString()
+        {
+            return "[" + Row.ToString() + ", " + Column.ToString() + "]";
+        }
+    }
+}
---
+++ Assignment2/Queue.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment2
+{
+    public class Queue<T>
+    {
+        public Node<T> Head;
+        public Node<T> Tail;
+        public int count;
+
+        public Queue()
+        {
+            this.Head = null;
+            this.count = 0;
+        }
+
+        public void Enqueue(T element)
+        {
+            Node<T> node = new Node<T>(element);
+            Node<T> currTail = Tail;
+            Tail = node;
+
+            if (count == 0)
+            {
+                Head = Tail;
+            }
+            else
+            {
+                currTail.Next = Tail;
+            }
+            count++;
+        }
+
+        public T Front()
+        {
+            if (this.Head != null)
+            {
+                Tail.SetNext(null);
+                return Head.GetElement();
+            }
+            throw new ApplicationException();
+        }
+        
+        public T Dequeue()
+        {
+            Node<T> currHead = Head;
+
+            if (count != 0)
+            {
+                this.Head = Head.Next;         
+                count--;
+                return currHead.GetElement();
+            }
+            else
+            {
+                throw new ApplicationException();
+            }
+        }
+
+        public void Clear()
+        {
+            count = 0;
+            Head = null;
+        }
+
+        public int GetSize()
+        {
+            return this.count;
+        }
+
+        public Node<T> GetHead()
+        {
+            return this.Head;
+        }
+
+        public Node<T> GetTail()
+        {
+            return this.Tail;
+        }
+
+        public bool IsEmpty()
+        {
+            return count == 0 ? true : false;
+        }
+    }
+}
